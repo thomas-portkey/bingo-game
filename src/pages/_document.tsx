b40555cf22601507f5232412ab59b894b1eaef79
Aaron <aron@portkey.finance>
2023-04-19 20:32:42 +0800

    feat: add rem to fix style

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
@@ -7,6 +8,24 @@ export default function Document() {
         <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1" />
       </Head>
       <body>
+        <Script id="fontsize" strategy="afterInteractive">
+          {`  
+            !(function(doc, win) {
+              var docEle = doc.documentElement,
+                event = "onorientationchange" in window ? "orientationchange" : "resize",
+                fn = function() {
+                  var width = docEle.clientWidth;
+                  var unitWidth = width < 1180 ? 375 : 1920;
+                  width && (docEle.style.fontSize = 10 * (width / unitWidth) + "px");
+                };
+
+              win.addEventListener(event, fn, false);
+              doc.addEventListener("DOMContentLoaded", fn, false);
+
+            }(document, window));     
+        `}
+        </Script>
+
         <Main />
         <NextScript />
       </body>
---
+++ src/pages/sourceMap.ts
@@ -0,0 +1,5 @@
+export default [
+  require('../../public/sand_clock.png').default.src,
+  require('../../public/bingo_white_pc.png').default.src,
+  require('../../public/bg_pc.png').default.src,
+];
